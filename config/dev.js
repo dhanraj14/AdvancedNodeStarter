147fe067cd8f78f254f6ba3e3f864b0493144b76
Dhanraj Rathinam <dhanrajrathinam@hotmail.com>
2023-01-10 09:40:10 +0530

    project demo

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://Dhanraj-node-adv:tQQa53NOHsPVZ9T7@ac-uehggjr-shard-00-00.lxawnm1.mongodb.net:27017,ac-uehggjr-shard-00-01.lxawnm1.mongodb.net:27017,ac-uehggjr-shard-00-02.lxawnm1.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-c75rfh-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
